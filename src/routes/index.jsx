81b4dc25c6201f4db116ea4fe2b38c57891539b1
Julian-D-Rojas <julian-d-rojas@hotmail.com>
2025-04-01 18:09:58 -0500

    fix import layout in routes/index

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,16 @@
-import { createBrowserRouter } from "react-router-dom"
-import { Layout } from "../layout/layout"
-import { Error } from "../pages/Error"
-import { Home } from "../pages/home"
-import { Information } from "../pages/Information"
+import { createBrowserRouter } from 'react-router-dom'
+import { Layout } from '../layout/Layout'
+import { Error } from '../pages/Error'
+import { Home } from '../pages/home'
+import { Information } from '../pages/Information'
 
-export const router = createBrowserRouter([{
+export const router = createBrowserRouter([
+  {
     element: <Layout />,
     errorElement: <Error />,
-    children: [{ path: `/`, element: <Home /> }, { path: `/information`, element: <Information /> }],
-}]) 
+    children: [
+      { path: `/`, element: <Home /> },
+      { path: `/information`, element: <Information /> }
+    ]
+  }
+])
