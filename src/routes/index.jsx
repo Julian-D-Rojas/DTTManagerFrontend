4d268f31d4f28c9d5491f2aca49a5a247f52a857
Julian-D-Rojas <julian-d-rojas@hotmail.com>
2025-04-01 18:16:23 -0500

    fix routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { Layout } from '../layout/Layout'
 import { Error } from '../pages/Error'
-import { Home } from '../pages/home'
+import { Home } from '../pages/Home'
 import { Information } from '../pages/Information'
 
 export const router = createBrowserRouter([
